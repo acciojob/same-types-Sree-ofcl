7fe7cffd826588d89446e81836211a8bcacbf2a0
Sree-ofcl <157797249+Sree-ofcl@users.noreply.github.com>
2024-06-19 13:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ function isSameType(value1, value2) {
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+function isSameType(value1, value2){
+	if (value1 === value2) {
+		console.log("true");
+	}
+	else{
+		console.log("false");
+	}
+}
 alert(isSameType(value1, value2));
