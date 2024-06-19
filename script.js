b1e0d7ea949a92a2120145a7db871a7ee2491465
Sree-ofcl <157797249+Sree-ofcl@users.noreply.github.com>
2024-06-19 18:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function isSameType(value1, value2) {
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
+//let value1 = prompt("Enter Start of the Range.");
+//let value2 = prompt("Enter End Of the Range.");
 function isSameType(value1, value2) {
   if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
+  } else if (Number.isNaN(value1) && typeof value2) {
+    return false;
+  } else if (value1 && Number.isNaN(value2)) {
+    return false;
   } else if (typeof value1 === typeof value2) {
     return true;
   } else {
     return false;
   }
 }
-alert(isSameType(value1, value2));
+alert( isSameType(value1, value2));
