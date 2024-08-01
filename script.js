1228e349788a9634bedfeed7e47618a9eab4ac62
Sree-ofcl <157797249+Sree-ofcl@users.noreply.github.com>
2024-08-01 14:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ function isSameType(value1, value2) {
 }
 
 // do not change the code below.
-//let value1 = prompt("Enter Start of the Range.");
-//let value2 = prompt("Enter End Of the Range.");
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+// Function to check if values are of the same type
 function isSameType(value1, value2) {
-  if (Number.isNaN(value1) && Number.isNaN(value2)) {
+  // Check if both values are NaN
+  if (isNaN(value1) && isNaN(value2)) {
     return true;
-  } else if (Number.isNaN(value1) && typeof value2) {
-    return false;
-  } else if (value1 && Number.isNaN(value2)) {
-    return false;
-  } else if (typeof value1 === typeof value2) {
-    return true;
-  } else {
-    return false;
+  }else if(isNaN(value1)){
+	  return false ;
+  } else if(isNaN(value2)){
+	  return false ;
   }
+
+  if (typeof value1 === typeof value2) {
+  	return true ;
+  }
+	return false ;
 }
-alert( isSameType(value1, value2));
+
+alert(isSameType(value1, value2));
+ 
